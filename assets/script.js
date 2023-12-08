b3fe518516cd7a62fdc9200fc54cb73ab61e82c9
Raymond Brien <raytbrien@gmail.com>
2023-12-08 11:18:45 +0000

    Add basic functions list to script.js

---
+++ assets/script.js
@@ -0,0 +1,73 @@
+// Wait for DOM to finish loading before running the game
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+    document.getElementById('answer-box').addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }     
+    })
+    runGame("addition");
+
+});
+
+
+// GLOBAL VARIABLES
+let totalscore = 0;
+let dayScore = 0;
+
+/**
+ * Calculates today's score only.
+ */
+function calculateDayScore() {
+
+}
+
+/**
+ * Calculates total score.
+ */
+function calculateTotalScore() {
+
+}
+
+/**
+ * Checks if total points are equal or greater to any of the growth points defined.
+ */
+function growPlant() {
+    let growthPoints = [1,2,3,4,6,8,10,12,14,16,18,20,23,26,30];
+
+
+}
+
+/**
+ * Finds the distance to the next growth points value from total points and displays to user.
+ */
+function untilNextGrowth() {
+
+}
+
+
+/**
+ * Event listener for taskboxes and adds relevant points, once completed by user, to total points score.
+ */
+function taskComplete() {
+
+}
+
+/**
+ * Allows user to reset the tasks at a specific time.
+ */
+ function setGoalResetTime() {
+    
+ }
