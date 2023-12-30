25687773ce4b5e90484903a5ed8baa11226f6412
Raymond Brien <raytbrien@gmail.com>
2023-12-30 14:11:10 +0000

    Reformat workspace to keep game seperate with new file game.html

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,3 +1,27 @@
+/**
+ * BS light theme set to document if user clicks light mode button on index.html
+ */
+document.getElementById('btn-check-5').addEventListener('change', function() {
+  console.log('checkbox clicked');
+
+  let colorCheckbox = document.getElementById("btn-check-5");
+  let theme = colorCheckbox.checked ? 'light' : 'dark';
+
+  console.log('currently theme is: ' + theme);
+
+  switch (theme) {
+      case 'light':
+          document.documentElement.setAttribute('data-bs-theme', 'light');
+          break;
+      case 'dark':
+          document.documentElement.setAttribute('data-bs-theme', 'dark');
+          break;
+  }
+});
+
+
+
+
 // credit Erikas Ramanauskas
 
 "use strict";
