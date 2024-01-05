601ec360fcecdfd183b719c7225794e61a196f18
Raymond Brien <raytbrien@gmail.com>
2024-01-05 17:48:02 +0000

    Add styles to game.html for improved discoverability and UI

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -12,13 +12,20 @@ document.getElementById('btn-check-5').addEventListener('change', function() {
   switch (theme) {
       case 'light':
           document.documentElement.setAttribute('data-bs-theme', 'light');
+          document.documentElement.style.setProperty('--bs-body-bg', '#fffce1');
+          document.documentElement.style.setProperty('--bs-body-color', '#3D3D3D');
+          document.documentElement.style.setProperty('--bs-emphasis-color', '#93B569');
+          document.documentElement.style.setProperty('--bs-secondary-color', '#2c2c2c');
           break;
       case 'dark':
           document.documentElement.setAttribute('data-bs-theme', 'dark');
+          document.documentElement.style.setProperty('--bs-body-bg', '#0e100f');
+          document.documentElement.style.setProperty('--bs-body-color', '#fffce1');
+          document.documentElement.style.setProperty('--bs-emphasis-color', '#93B569');
+          document.documentElement.style.setProperty('--bs-secondary-color', '#BDBDBD');
           break;
   }
 });
-
 
 
 
